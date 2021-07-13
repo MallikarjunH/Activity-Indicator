bca68ae1e2c5e52156ff8a94781f7680f9cbfe85
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-07-13 16:47:40 +0530

    update

--- ActivityIndicatorView-Swift/ActivityIndicatorView-Swift/ViewController.swift
+++ ActivityIndicatorView-Swift/ActivityIndicatorView-Swift/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var spinner = UIActivityIndicatorView(style: .gray)
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        spinner.translatesAutoresizingMaskIntoConstraints = false
+        //Way 1
+       /* spinner.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(spinner)
         spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true */
+        
+        //way 2
+        spinner.center = view.center
+        view.addSubview(spinner)
     }
 
     
